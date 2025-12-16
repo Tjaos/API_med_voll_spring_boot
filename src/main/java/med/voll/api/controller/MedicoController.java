4b5cfa4157a29b472733994a6b22b1bb807769f0
Tjaos <th83287716@gmail.com>
2025-12-16 12:04:58 -0300

    feat: implementação do controlador de médico, DTO para criar um objeto dos dados a ser enviado e Enum de especialidades

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-    private String nome;
 
     @PostMapping
-    public void cadastrar(@RequestBody String json){
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
     }
 
 
